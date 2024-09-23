06820b022dfe0d843057c3797fd9e69779978e1e
parien2022 <par22002@byui.edu>
2024-09-23 03:52:06 -0300

    CRUD operations successfully added

--- controllers/clothesController.ts
+++ controllers/clothesController.ts
@@ -27,9 +27,111 @@ class ClothesController {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(clothes);
         }catch (err){
-            res.status(400).json({ message: err instanceof Error ? err.message : 'Unknown error'});
+            res.status(400).json({ message: err instanceof Error ? err.message : 'Error getting all clothess'});
         }
     }
+
+    public async getById(req: Request, res: Response): Promise<any> {
+        try{
+            await this.initializeDb();
+            if(!this.db){
+                throw new Error('Database not initialized');
+            }
+            else if(!ObjectId.isValid(req.params.id)){
+                return res.status(400).json({message: 'Must use a valid clothes id'});
+            }
+
+            const clothesId = new ObjectId(req.params.id);
+            const clothes = await this.db.collection('clothes').find({_id: clothesId}).toArray();
+
+            if (clothes.length == 0){
+                return res.status(404).json({message: 'Clothes not found'});
+            }
+
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(clothes[0]);
+        }catch (err){
+            res.status(400).json({ message: err instanceof Error ? err.message : 'Error getting Clothes by id'});
+        }
+    }
+
+    public async createClothes(req: Request, res: Response): Promise<any> {
+        try{
+            await this.initializeDb();
+            if(!this.db){
+                throw new Error('Database not initialized');
+            }
+
+            const clothes = {
+                "name": req.body.name,
+                "size": req.body.size,
+                "color": req.body.color,
+                "price": req.body.price,
+                "inStock": req.body.inStock
+            }
+
+            const response = await this.db.collection('clothes').insertOne(clothes);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.status(201).json({message: 'Successfully Created', _id: response.insertedId});
+        }catch (err){
+            res.status(500).json({ message: err instanceof Error ? err.message : 'Error creating clothes'});
+        }
+    }
+
+    public async updateClothes(req: Request, res: Response): Promise<any> {
+        try{
+            await this.initializeDb();
+            if(!this.db){
+                throw new Error('Database not initialized');
+            }
+            else if(!ObjectId.isValid(req.params.id)){
+                return res.status(400).json({message: 'Must use a valid clothes id'});
+            }
+
+            const clothes = {
+                "name": req.body.name,
+                "size": req.body.size,
+                "color": req.body.color,
+                "price": req.body.price,
+                "inStock": req.body.inStock
+            }
+
+            const clothesId = new ObjectId(req.params.id);
+            const response = await this.db.collection('clothes').replaceOne({_id: clothesId}, clothes);
+
+            res.setHeader('Content-Type', 'application/json');
+            if (response.modifiedCount > 0){
+                res.status(200).json({message: 'Successfully updated'});
+            }
+        }catch (err){
+            res.status(500).json({ message: err instanceof Error ? err.message : 'Error updating clothes'});
+        }
+    }
+
+    public async deleteClothes(req: Request, res: Response): Promise<any> {
+        try{
+            await this.initializeDb();
+            if(!this.db){
+                throw new Error('Database not initialized');
+            }
+            else if(!ObjectId.isValid(req.params.id)){
+                return res.status(400).json({message: 'Must use a valid clothes id'});
+            }
+
+            const clothesId = new ObjectId(req.params.id);
+            const response = await this.db.collection('clothes').deleteOne({_id: clothesId});
+
+            res.setHeader('Content-Type', 'application/json');
+            if (response.deletedCount > 0){
+                res.status(200).json({message: 'Successfully deleted'});
+            }
+        }catch (err){
+            res.status(500).json({ message: err instanceof Error ? err.message : 'Error deleting clothes'});
+        }
+    }
+
+    
 }
 
 export default new ClothesController();
--- routes/ClothesRoutes.ts
+++ routes/ClothesRoutes.ts
@@ -12,7 +12,11 @@ class ClothesRoutes {
     }
 
     private initializeRoutes() {
-        this.router.get('/clothes', (req: Request, res: Response) => ClothesController.getAll(req, res))
+        this.router.get('/getAll', (req: Request, res: Response) => ClothesController.getAll(req, res));
+        this.router.get('/getById/:id', (req: Request, res: Response) => ClothesController.getById(req, res));
+        this.router.post('/createClothes/', (req: Request, res: Response) => ClothesController.createClothes(req, res));
+        this.router.put('/updateClothes/:id', (req: Request, res: Response) => ClothesController.updateClothes(req, res));
+        this.router.delete('/deleteClothes/:id', (req: Request, res: Response) => ClothesController.deleteClothes(req, res));
     }
     
 }
